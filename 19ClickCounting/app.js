941941a497f59e766d465dd1095672620208d475
Nevo0 <Piecyk.rafal@gmail.com>
2018-12-18 14:47:57 +0100

    Add component

--- 19ClickCounting/app.js
+++ 19ClickCounting/app.js
@@ -2,11 +2,14 @@ class Counter extends React.Component {
   state = {
     count: 0,
     result: this.props.counter
+    // do result i do elseif tez przypisujemy props ktory jest w ReactDOM moze byc tez jako zmienna
   };
 
-  handleMathClick(type, number) {
+  handleMathClick = (type, number) => {
+    // musi byc funkcja strzalkowa zeby dzialalo poprawnie this
     // debugger;
-    if (type === "substraction") {
+
+    if (type === "subtraction") {
       this.setState(prevState => ({
         count: prevState.count + 1,
         result: prevState.result - number
@@ -19,32 +22,59 @@ class Counter extends React.Component {
     } else if (type === "reset") {
       this.setState(prevState => ({
         count: prevState.count + 1,
-        result: 0
+        result: this.props.counter
       }));
     }
-  }
+  };
 
   render() {
     return (
       <React.Fragment>
-        <button onClick={this.handleMathClick.bind(this, "substraction", 10)}>
-          -10
-        </button>
-        <button onClick={() => this.handleMathClick("substraction", 1)}>
-          -1
-        </button>
-        <button onClick={this.handleMathClick.bind(this, "reset")}>
-          Reset
-        </button>
-        <button onClick={() => this.handleMathClick("addition", 1)}>+1</button>
-        <button onClick={() => this.handleMathClick("addition", 10)}>
-          +10
-        </button>
-
+        <MathButton
+          name="-10"
+          number="10"
+          type="subtraction"
+          click={this.handleMathClick}
+        />
+        <MathButton
+          name="-1"
+          number="1"
+          type="subtraction"
+          click={this.handleMathClick}
+        />
+        <MathButton
+          name="Reset"
+          number="10"
+          type="reset"
+          click={this.handleMathClick}
+        />
+        <MathButton
+          name="+1"
+          number="1"
+          type="addition"
+          click={this.handleMathClick}
+        />
+        <MathButton
+          name="+10"
+          number="10"
+          type="addition"
+          click={this.handleMathClick}
+        />
         <h1>Liczna Kliknieć: {this.state.count}</h1>
         <h1>Wynik: {this.state.result}</h1>
       </React.Fragment>
     );
   }
 }
+
+const MathButton = props => {
+  const number = parseInt(props.number);
+  console.log(props);
+  // return <button onClick={props.click}>{props.name}</button>;nie zadziala bo nie przekazujemy nic do handleMathClick musimy użyć funkcj strzalkowej
+  return (
+    <button onClick={() => props.click(props.type, number)}>
+      {props.name}
+    </button>
+  );
+};
 ReactDOM.render(<Counter counter="10" />, document.getElementById("root"));
