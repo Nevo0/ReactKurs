211833ba912049cdbd809c2309f0827866c30b5d
Nevo0 <Piecyk.rafal@gmail.com>
2019-01-11 15:44:12 +0100

    Metoda filter

--- 38PracaZTablicami/app.js
+++ 38PracaZTablicami/app.js
@@ -1,15 +1,57 @@
-const Item = props => {
-  return <li>{`Owoc ${props.content}`}</li>;
+const date = {
+  users: [
+    {
+      id: 1,
+      age: 29,
+      name: "Arek",
+      sex: "male"
+    },
+    {
+      id: 2,
+      age: 19,
+      name: "Marta",
+      sex: "female"
+    },
+    {
+      id: 3,
+      age: 46,
+      name: "Stasia",
+      sex: "female"
+    },
+    {
+      id: 4,
+      age: 19,
+      name: "Karol",
+      sex: "male"
+    },
+    {
+      id: 5,
+      age: 19,
+      name: "Zosia",
+      sex: "female"
+    }
+  ]
+};
+const Item = ({ content }) => {
+  // możęmy wpisac propsy o jaknie nam chodzi, a nie przekazywać wszystkich propsow
+  return (
+    <div className="userInfo">
+      {/* przekazujemy propsy o nazwie takiej jak w items */}
+      <h1>{content.name}</h1>
+      <p>Informacje o użytkowniku</p>
+      <p>
+        Wiek użytkowaika: <strong>{content.age}</strong>
+      </p>
+      <p>Płeć użytkownika: {content.sex}</p>
+    </div>
+  );
 };
 
 class LiistItem extends React.Component {
-  state = {
-    items: ["jabłko", "sliwka", "gruszka"]
-  };
+  // state = {
+  //   items: ["jabłko", "sliwka", "gruszka"]
+  // };
   render() {
-    const Items = this.state.items.map(item => (
-      <Item key={item} content={item} />
-    ));
     return (
       <div>
         <ul>{Items}</ul>
@@ -18,4 +60,4 @@ class LiistItem extends React.Component {
   }
 }
 
-ReactDOM.render(<LiistItem />, document.getElementById("root"));
+ReactDOM.render(<LiistItem date={date} />, document.getElementById("root"));
