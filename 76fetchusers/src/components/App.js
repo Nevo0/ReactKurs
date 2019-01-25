58c37afd11480029579b156f9a11909315d908f3
Nevo0 <Piecyk.rafal@gmail.com>
2019-01-25 13:13:47 +0100

    tu Upercase

--- 76fetchusers/src/components/App.js
+++ 76fetchusers/src/components/App.js
@@ -3,15 +3,36 @@ import "./App.css";
 import ButtonFetchUsers from "./ButtonFetchUsers";
 import UsersList from "./UsersList";
 
+const API = "https://randomuser.me/api/?results=5";
+
 class App extends Component {
   state = { users: null };
 
   handleDataFetch = () => {
-    console.log("click");
+    fetch(API)
+      .then(response => {
+        // console.log(response.ok);
+
+        if (response.ok) {
+          return response;
+        }
+        throw Error("Błą!!!!!!!!");
+      })
+      .then(response => response.json())
+      .then(
+        data =>
+          this.setState({
+            users: data.results
+          })
+        // console.log(data.results)
+      )
+      .catch(error => console.log(error));
   };
 
   render() {
     const users = this.state.users;
+    console.log(this.state.users);
+
     return (
       <div>
         <ButtonFetchUsers click={this.handleDataFetch} />
--- 76fetchusers/src/components/UsersList.js
+++ 76fetchusers/src/components/UsersList.js
@@ -3,13 +3,25 @@
 import React from "react";
 import "./UsersList.css";
 const UsersList = props => {
-  console.log(props.users);
+  const usersItem = props.users.map(user => {
+    {
+      console.log(user.login.uuid);
+      let title =
+        user.name.title.charAt(0).toUpperCase() + user.name.title.slice(1);
+      let first =
+        user.name.first.charAt(0).toUpperCase() + user.name.first.slice(1);
+      let last =
+        user.name.last.charAt(0).toUpperCase() + user.name.last.slice(1);
 
-  return (
-    <ul>
-      <li>{props.users}</li>
-    </ul>
-  );
+      return (
+        <li key={user.login.uuid}>
+          {title} {first} {last}
+        </li>
+      );
+    }
+  });
+
+  return <ul>{usersItem}</ul>;
 };
 
 export default UsersList;
