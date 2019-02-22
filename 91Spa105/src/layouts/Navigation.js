e48ff1998e6284f705c9aa46806e4cbfe372baef
Nevo0 <Piecyk.rafal@gmail.com>
2019-02-22 21:21:55 +0100

    Navigation css

--- 91Spa105/src/layouts/Navigation.js
+++ 91Spa105/src/layouts/Navigation.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/Navigation.css";
+
+const list = [
+  { name: "start", path: "/" },
+  { name: "products", path: "products" },
+  { name: "contact", path: "contact" },
+  { name: "Panel administratora", path: "admin" }
+];
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  const menu = list.map(item => (
+    <li key={item.name}>
+      <NavLink to={item.path}>{item.name}</NavLink>
+    </li>
+  ));
+
+  return (
+    <nav className="main">
+      <ul>{menu}</ul>
+    </nav>
+  );
 };
 
 export default Navigation;
