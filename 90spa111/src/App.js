b32c356e3a571a824b0102c63db406ecffc0ecb3
Nevo0 <Piecyk.rafal@gmail.com>
2019-02-22 20:11:46 +0100

    NavLink Route Swich

--- 90spa111/src/App.js
+++ 90spa111/src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter, NavLink, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+const Home = () => <h1>Strona Home</h1>;
+const News = () => <h1>Strona News</h1>;
+const Contact = () => <h1>Strona Contact</h1>;
+
+const ErrorPage = () => <h1>Strona nie istnieje</h1>;
 
 class App extends Component {
   state = {};
@@ -12,11 +19,39 @@ class App extends Component {
             <nav>
               <ul>
                 <li>
-                  <Link to="/">Start</Link>
+                  <NavLink
+                    to="/"
+                    exact
+                    activeClassName="home_selected"
+                    activeStyle={{
+                      backgroundColor: "gray",
+                      letterSpacing: "6px"
+                    }}
+                  >
+                    Start
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/news" activeClassName="news_selected">
+                    Aktualności
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/contact" activeClassName="contact_selected">
+                    Kontakt
+                  </NavLink>
                 </li>
               </ul>
             </nav>
           </header>
+          <section>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/news" component={News} />
+              <Route path="/contact" component={Contact} />
+              <Route component={ErrorPage} />
+            </Switch>
+          </section>
         </div>
       </BrowserRouter>
     );
