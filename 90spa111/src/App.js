7672754644b2803072bbf2d40032b235361ce33b
Nevo0 <Piecyk.rafal@gmail.com>
2019-02-22 18:48:05 +0100

    Start with browserrouter

--- 90spa111/src/App.js
+++ 90spa111/src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
+import { BrowserRouter, Link } from "react-router-dom";
 
 class App extends Component {
   state = {};
 
   render() {
-    return <div>XXX</div>;
+    return (
+      <BrowserRouter>
+        <div>
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Start</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
--- 90spa111/src/index.js
+++ 90spa111/src/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // unregister() to register()
 // http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
